dff1c0b4b7d5511f1044a917eef7f3df04d4ffbd
comsavvy <olusolaogundepo@gmail.com>
2020-08-21 12:29:06 +0100

    Introduction of Concurrent in guessing game
    I introduced the main function to the code and I split some codes into
    function. And I also used if __name__ == '__main__' to the
    guessing_game.py in order to this in order module of my python codes

--- guessing_game.py
+++ guessing_game.py
@@ -1,4 +1,14 @@
 import random as rd
+from concurrent.futures import ThreadPoolExecutor
+
+
+def prompt(justDisplayTimeLeft):
+    if justDisplayTimeLeft != 0:
+        print('Too long, try again!')
+    else:
+        print('Too long')
+
+
 def guessingFuc():
     secret_number= rd.randint(1, 21)
     guess_count= 1
@@ -8,38 +18,42 @@ def guessingFuc():
             guess= int(input('Guess: '))
             justDisplayTimeLeft= timeLeft-1
             if guess > secret_number:
-                if justDisplayTimeLeft is not 0: 
-                    print('Too long, try again!')
-                else:
-                    print('Too long')
+                prompt(justDisplayTimeLeft)
             elif guess < secret_number:                
-                if justDisplayTimeLeft is not 0: 
-                    print('Too small, try again!')
-                else:
-                    print('Too small')
+                prompt(justDisplayTimeLeft)
             else:
                 break
             guess_count += 1        
-            if justDisplayTimeLeft is not 0:
+            if justDisplayTimeLeft != 0:
                 print(f"Time left is {justDisplayTimeLeft}")
             else:
                 pass
             timeLeft= timeLeft-1
         except ValueError:
             print('Invalid pin')
-        except TypeError:
-            print('Invalid type!')
+            break
     if secret_number is guess:
         print('You Won!')
     else:
         print ('You lose!')
+
+
+def rerun():
+    with ThreadPoolExecutor(max_workers=2) as executor:
+        executor.submit(guessingFuc)
+
+
+def main():
+    rerun()
+    while True:
+        play= input("Do you want to play again? [Y/N] ").capitalize().startswith('Y')
+        if play:
+            rerun()
+        else:
+            break
+
+
 #Game start here
 print("Hello! I have number between 1-20 \nCan you guess my number? \nBest of luck!")
 if __name__ == "__main__":
-    guessingFuc()
-    while True:
-        play= input("Do you want to play again? [Y/N] ").capitalize()
-        if play=='Y' or play== 'Yes':
-            guessingFuc()
-        else:
-            break
+    main()
